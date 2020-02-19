8206824cd96dd712d83f3ee4e40233ef2d23ca88
Baruch Phillips <baruchphillips@gmail.com>
2020-02-19 11:13:06 -0500

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all 
+Writer.destroy_all
+
+jaysonStark = Writer.create(first_name: 'Jayson', last_name: 'Stark')
+busterOlney = Writer.create(first_name: 'Buster', last_name: 'Olney')
+kenRosenthal = Writer.create(first_name: 'Ken', last_name: 'Rosenthal')
+
+article1 = Article.create(title: "Mookie Trade", content: "Sucks", writer: kenRosenthal)
+article2 = Article.create(title: "Lester Trade", content: "Also Sucks", writer: kenRosenthal)
+article3 = Article.create(title: "Gerrit Cole signing", content: "Yikes", writer: jaysonStark)
