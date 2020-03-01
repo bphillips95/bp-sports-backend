dafb9604d36bc2cfcc1d7fc19e263bd592d32df3
Baruch Phillips <baruchphillips@gmail.com>
2020-03-01 18:59:19 -0500

    validations in user file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :articles
-    # validates :username, uniqueness: { case_sensitive: false }
+    validates :username, uniqueness: { case_sensitive: false }
+    validates :password, length: { in: 6..20 }
+    # app crashes with validations
+    # validates :first_name, presence: true
+    # validates :last_name, presense: true
     def full_name
         [first_name, last_name].join(' ')
     end
